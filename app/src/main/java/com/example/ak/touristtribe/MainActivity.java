9f37762f85074b5a82aed9cfde9be0bebecd354e
abhishekjha007 <abhi.281097@gmail.com>
2020-06-29 11:46:13 +0530

    Android Project

---
+++ app/src/main/java/com/example/ak/touristtribe/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.ak.touristtribe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView wv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        wv=(WebView)findViewById(R.id.webView);
+
+        wv.getSettings().setLoadsImagesAutomatically(true);
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+
+        wv.setWebViewClient(new WebViewClient());
+
+        wv.loadUrl("http://seller.touristtribe.in");
+
+    }
+}
